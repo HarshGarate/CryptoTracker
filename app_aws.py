4ab12f5a4e8f85345ffc22b0d1a28bae888273a5
HarshGarate <115451513+HarshGarate@users.noreply.github.com>
2026-02-06 19:43:53 +0530

    Enabled debug mode for AWS

--- app_aws.py
+++ app_aws.py
@@ -193,4 +193,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=5000)
+    application.run(host='0.0.0.0', port=5000, debug=True)
